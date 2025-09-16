be46fd1043dd89bdf78242aaba5831f64669044d
KrzysiekKropka <91200856+KrzysiekKropka@users.noreply.github.com>
2025-09-16 21:54:57 +0200

    Adding overriding STREAMS.DAT and ASSETS.DAT functionality

---
+++ tool.py
@@ -0,0 +1,93 @@
+import os
+import platform
+import subprocess
+import shutil
+
+BASE_FOLDER = os.getcwd()
+NEW_RSTM_FOLDER = os.path.join(BASE_FOLDER, "NEW_RSTM")
+MOD_FOLDER = os.path.join(BASE_FOLDER, "MOD")
+CONTENTS_FOLDER = os.path.join(BASE_FOLDER, "DAT_CONTENTS")
+TOOLS_FOLDER = os.path.join(BASE_FOLDER, "external_tools")
+
+if platform.system() == "Windows":
+    ffmpeg_bin = os.path.join(TOOLS_FOLDER, "ffmpeg.exe")
+else:
+    ffmpeg_bin = "ffmpeg"
+
+RED = "\033[31m"
+GREEN = "\033[32m"
+BLUE = "\033[34m"
+YELLOW = "\033[33m"
+RESET = "\033[0m"
+
+def decompile_dats():
+    if os.path.exists(os.path.join(BASE_FOLDER, "ASSETS.DAT")):
+        print(f"{YELLOW}Decompiling ASSETS.DAT...{RESET}")
+        subprocess.run(["python", os.path.join(TOOLS_FOLDER, "dave.py"), "X", "ASSETS.DAT", "-o", os.path.join(CONTENTS_FOLDER, "ASSETS")], check=True)
+
+    if os.path.exists(os.path.join(BASE_FOLDER, "STREAMS.DAT")):
+        print(f"{YELLOW}Decompiling STREAMS.DAT...{RESET}")
+        subprocess.run(["python", os.path.join(TOOLS_FOLDER, "hash_build.py"), "X", "STREAMS.DAT", "-o", os.path.join(CONTENTS_FOLDER, "STREAMS"), "-nl", os.path.join(TOOLS_FOLDER, "STREAMS.LST"), "-a", "mclub", "-th", "45"])
+
+def convert_to_wav():
+    for file in os.listdir(NEW_RSTM_FOLDER):
+        filename, ext = os.path.splitext(file)
+        if ext.lower() != ".wav" and ext.lower() != ".rsm":
+            ext_path = os.path.join(NEW_RSTM_FOLDER, f"{file}")
+            filename, _ = os.path.splitext(file)
+            wav_path = os.path.join(NEW_RSTM_FOLDER, f"{filename}.wav")
+            print(f"Converting {file} → {filename}.wav")
+            subprocess.run([
+                ffmpeg_bin, '-y', '-i', ext_path,
+                '-vn', '-sn', '-ac', '2', '-ar', '44100', '-acodec', 'pcm_s16le',
+                wav_path
+            ], check=True)
+            os.remove(ext_path)
+
+def convert_to_rstm():
+    for file in os.listdir(NEW_RSTM_FOLDER):
+        filename, ext = os.path.splitext(file)
+        if ext.lower() == ".wav":
+            wav_path = os.path.join(NEW_RSTM_FOLDER, f"{file}")
+            filename, _ = os.path.splitext(file)
+            print(f"Converting {file} → {filename}.rsm")
+            subprocess.run(['python', os.path.join(TOOLS_FOLDER, "rstm_build.py"), wav_path], check=True)
+            os.remove(wav_path)
+
+def compile_dats():
+    print(f"{YELLOW}Compiling ASSETS.DAT...{RESET}")
+    subprocess.run(["python", os.path.join(TOOLS_FOLDER, "dave.py"), "B", "-ca", "-cn", "-cf", "-fc", "1", "ASSETS", "ASSETS.DAT"], check=True)
+
+    print(f"{YELLOW}Compiling STREAMS.DAT...{RESET}")
+    subprocess.run(["python", os.path.join(TOOLS_FOLDER, "hash_build.py"), "B", "STREAMS", "STREAMS.DAT", "-a", "MClub"], check=True)
+
+def mod_dat():
+    for folder in ["ASSETS", "STREAMS"]:
+        mod_path = os.path.join(MOD_FOLDER, folder)
+        target_path = os.path.join(CONTENTS_FOLDER, folder)
+
+        if os.path.isdir(mod_path):
+            print(f"{YELLOW}Applying mods from {mod_path} → {target_path}{RESET}")
+            for root, dirs, files in os.walk(mod_path):
+                rel_path = os.path.relpath(root, mod_path)
+                dest_dir = os.path.join(target_path, rel_path)
+                os.makedirs(dest_dir, exist_ok=True)
+
+                for file in files:
+                    src_file = os.path.join(root, file)
+                    dst_file = os.path.join(dest_dir, file)
+                    shutil.copy2(src_file, dst_file)
+
+def main():
+    answer = input(f"{BLUE}Do you want to decode STREAMS.DAT and ASSETS.DAT? {RESET}(Y/N) ").strip().lower()
+    if answer == "y":
+        decompile_dats()
+    convert_to_wav()
+    convert_to_rstm()
+    answer = input(f"{BLUE}Do you want to encode STREAMS and ASSETS? {RESET}(Y/N) ").strip().lower()
+    if answer == "y":
+        compile_dats()
+    
+
+if __name__ == "__main__":
+    main()
